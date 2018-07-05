3388bb1badd66cd3da3754c3d69468ec3655c611
bitflower <matthias.max@bitflower.net>
2018-07-05 20:17:15 +0200

    Better solution

--- src/components/dynamic-container/dynamic-container.tsx
+++ src/components/dynamic-container/dynamic-container.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Prop } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
 
 @Component({
     tag: 'dynamic-container',
@@ -6,39 +6,10 @@ import { Component, Element, Prop } from '@stencil/core';
     shadow: true
 })
 export class DynamicContainer {
-
-    @Element() el: HTMLStencilElement;
-
-    @Prop() component: string;
     @Prop() props: any;
+    @Prop() component: string;
 
     render() {
-
-        let c: HTMLElement = document.createElement(this.component.toUpperCase());        // Create a <button> element
-
-        if (this.props) {
-            Object.keys(this.props).forEach((key: string) => {
-                console.log(`${key}: ${this.props[key]}`);
-                // c.attributes[key] = this.props[key];
-                // c.attributes.setNamedItem(key) = this.props[key];
-                c.setAttribute(key, this.props[key]);
-            });
-        }
-
-        // for (let p in this.props) {
-        // c.attributes = this.props
-        // }
-
-        this.el.shadowRoot.appendChild(c);
-
-        // WOrks
-        // document.body.appendChild(c);  
-
-
-
-        return (
-            <div>
-            </div>
-        );
+        return h(this.component, { ...this.props });
     }
 }
